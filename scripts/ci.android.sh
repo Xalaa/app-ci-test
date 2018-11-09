3454bddb675ef9831071bdfd2dcbe647b57ae1bf
Jacob Greenberg <jacob@greenberg.nu>
2018-11-09 09:48:20 -0800

    add travis scripts

---
+++ scripts/ci.android.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+source $(dirname "$0")/ci.sh
+
+pushd detox/android
+run_f "./gradlew test"
+popd
+
+pushd detox/test
+# Workaround until react android issue will be fixed - react-native: 0.55
+mv node_modules/react-native/ReactAndroid/release.gradle node_modules/react-native/ReactAndroid/release.gradle.bak
+cp extras/release.gradle node_modules/react-native/ReactAndroid/
+
+run_f "npm run build:android"
+run_f "npm run e2e:android -- --headless --no-color"
+run_f "npm run verify-artifacts:android"
+popd
